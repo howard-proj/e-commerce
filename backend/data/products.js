97b2c884d520390b5d658196b1179bf4bd147a32
Howard J <howardjumino153@gmail.com>
2021-02-19 16:26:48 +1100

    added backend configurations, express and products

---
+++ backend/data/products.js
@@ -0,0 +1,37 @@
+const products = [
+  {
+    id: "1",
+    name: "Roland TD-1DMK V-Drums Electronic Drum Kit",
+    image: "/img/roland.png",
+    brand: "Roland",
+    category: "Electronics",
+    description: `If you’ve always dreamed of playing drums, you’ll know it’s a powerful ambition. But along with the excitement, you may also feel that learning drums is too hard. And yet, while you certainly do have to put in some effort, becoming a tight drummer may not be as tough as you think. If you can tap in time to music, you already have an intuitive understanding of the fundamentals of rhythm...you just need to take the next step. Roland V-Drums are the world’s most popular electronic drums, thanks to their powerful sound, great playing feel and legendary durability. And if you think you might have drumming in your blood, the new V-Drums TD-1DMK will help you find it. The TD-1DMK provides a fulfilling drumming experience in a compact kit that’s easy to move around. The simple-yet-powerful module includes a variety of expressive drum kits, connected to Roland’s renowned dual-mesh pads for snare and toms which match the feel of playing on acoustic drums but without the noise* – great for late-night practice sessions. You can play with headphones too, so you won’t disturb family or neighbors, and with onboard coach functions and the ability to connect to Melodics drum training software, you’ll be surprised how quickly you’ll improve. Pick up the sticks, take a deep breath and pull up to the TD-1DMK. A lifetime of drumming awaits – so what are you waiting for?`,
+    price: 1049.0,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 5,
+  },
+
+  {
+    id: "2",
+    name: "Yamaha DTX452K Electronic Drum Kit",
+    image: "/img/yamaha.jpeg",
+    brand: "Yamaha",
+    category: "Electronics",
+    description: `The DTX452K contains a DTX452 drum kit w/bass drum pedal. Add the value add-on bundle option for everything you need to start playing, or select your own options from our range of accessories.
+
+    • Easy setup with rack pre-assembled so you spend less time setting up and more time playing!
+    • DTX452K model upgrades you to a kick trigger pad w/single kick pedal, HH65 hi-hat pedal and 3-Zone Snare Pad
+    • Powerful new easy to navigate module with 415 great on-board stereo voicings
+    • New choke-able crash cymbal pad
+    • Redesigned rack with 4 legs for stability – able to be setup left handed!
+    • New DTX402 Touch app - compatible with iOS and Android devices.
+    • Practice, Record and Share your performances with Yamaha’s Rec’n’Share app`,
+    price: 999.5,
+    countInStock: 3,
+    rating: 5,
+    numReviews: 2,
+  },
+];
+
+module.exports = products;
