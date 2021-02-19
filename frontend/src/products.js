6ee23819739e3a82e81f94af8a68d379533e6bc5
Howard J <howardjumino153@gmail.com>
2021-02-19 14:11:09 +1100

    added dummy products, product components, images and HomeScreens

---
+++ frontend/src/products.js
@@ -0,0 +1,27 @@
+const products = [
+  {
+    id: "1",
+    name: "Roland TD-1DMK V-Drums Electronic Drum Kit",
+    image: "/img/roland.png",
+    brand: "Roland",
+    category: "Electronics",
+    price: 1049.0,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 5,
+  },
+
+  {
+    id: "2",
+    name: "Yamaha DTX452K Electronic Drum Kit",
+    image: "/img/yamaha.jpeg",
+    brand: "Yamaha",
+    category: "Electronics",
+    price: 999.5,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 2,
+  },
+];
+
+export default products;
