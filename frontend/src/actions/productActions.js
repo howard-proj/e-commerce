3a16db2a0f1b88afcbfd398f754e2f8d6870de1c
Howard J <howardjumino153@gmail.com>
2021-02-24 16:16:42 +1100

    added message and loader components

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -8,7 +8,7 @@ import axios from "axios";
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get("api/products");
+    const { data } = await axios.get("/api/products");
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
