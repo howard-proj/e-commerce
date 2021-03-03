b2404ef8cfb62336a167bde33fb9bb4b1122e9dc
Howard J <howardjumino153@gmail.com>
2021-03-03 21:07:42 +1100

    added router to get order by ID endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -34,4 +34,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder);
   }
 });
-export { addOrderItems };
+
+// @description: Get order by ID
+// @route: GET /api/orders/:id
+// @access: Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await (await Order.findById(req.params.id)).populated(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    rs.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById };
